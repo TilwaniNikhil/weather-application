11a79806c409fd84922807a5ad50fab106e8b8df
Nikhil Tilwani <nikhiltilwani.nt@gmail.com>
2024-04-27 00:52:42 +0530

    comit

---
+++ src/Data/Data.jsx
@@ -0,0 +1,38 @@
+export const Data = [
+  {
+    country: "India",
+    name: "Mumbai",
+    lat: "34.1463",
+    lng: "73.21168",
+  },
+  {
+    country: "PK",
+    name: "Adilpur",
+    lat: "27.93677",
+    lng: "69.31941",
+  },
+  {
+    country: "US",
+    name: "California",
+    lat: "29.14269",
+    lng: "71.25771",
+  },
+  {
+    country: "France",
+    name: "Paris",
+    lat: "30.67791",
+    lng: "71.74344",
+  },
+  {
+    country: "Bhutan",
+    name: "Thimphu",
+    lat: "34.00337",
+    lng: "72.12561",
+  },
+  {
+    country: "Bangladesh",
+    name: "Dhaka",
+    lat: "36.30703",
+    lng: "74.61545",
+  },
+]
